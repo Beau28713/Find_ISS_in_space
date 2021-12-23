ba432a1f0d7dae45f124e9d9fb79b43469c2cd44
Beau <beau28713@gmail.com>
2021-12-22 20:38:41 -0500

    added error catching for api beong down

--- main.py
+++ main.py
@@ -24,28 +24,37 @@ iss = typer.Typer()
 @iss.command()
 def get_people_craft(): 
     """Prints out who is currently in space and the what space craft they are on """
+    try:
+        returned_data = requests.get("http://api.open-notify.org/astros.json")
 
-    returned_data = requests.get("http://api.open-notify.org/astros.json")
+        typer.echo(f"The status code is: {returned_data.status_code}")
 
-    typer.echo(f"The status code is: {returned_data.status_code}")
+        data_dict = returned_data.json()
 
-    data_dict = returned_data.json()
+        for people in data_dict["people"]:
+            typer.echo(f"{people['name']} is on the {people['craft']}")
+    
+    except BaseException as error:
+        typer.echo("The error was:")
+        typer.echo(f"{error}")
 
-    for people in data_dict["people"]:
-        typer.echo(f"{people['name']} is on the {people['craft']}")
+    
 
 @iss.command()
 def get_current_location():
     """Prints out the current longitude and latitud location of the ISS space station"""
+    try:
+        returned_data = requests.get("http://api.open-notify.org/iss-now.json")
 
-    returned_data = requests.get("http://api.open-notify.org/iss-now.json")
+        typer.echo(f"The status code is: {returned_data.status_code}")
 
-    typer.echo(f"The status code is: {returned_data.status_code}")
+        data_dict = returned_data.json()
+        typer.echo(f'The ISS latitude position is : {data_dict["iss_position"]["latitude"]}')
+        typer.echo(f'The ISS longitude position is : {data_dict["iss_position"]["longitude"]}')
 
-    data_dict = returned_data.json()
-    typer.echo(f'The ISS latitude position is : {data_dict["iss_position"]["latitude"]}')
-    typer.echo(f'The ISS longitude position is : {data_dict["iss_position"]["longitude"]}')
-
+    except BaseException as error:
+        typer.echo("The error was:")
+        typer.echo(f"{error}")
 
 if __name__ == "__main__":
     iss()
